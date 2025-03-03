cc439405a6b2e4cb9a39a3ec337b5ed640e5452b
tmura-zzz <tmura_zzz@da2.so-net.ne.jp>
2025-03-03 11:10:32 +0900

    test

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<main>
+    <div class="bg-red-200">メニューバー</div>
+    <div class="bg-green-200">問題</div>
+    <div class="bg-blue-200">
+        <button>選択肢１</button>
+        <button>選択肢２</button>
+        <button>選択肢３</button>
+    </div>
+</main>
